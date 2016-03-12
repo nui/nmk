5b55bcddd89e86ced85bd7e8d1ce0788e45695b9
Nui Narongwet <narongwet.m@gmail.com>
2016-03-12 16:03:27 +0700

    Don't write if Pathogen is up-to-date

--- tools/update.js
+++ tools/update.js
@@ -1,17 +1,37 @@
-import Promise from 'bluebird';
-import request from 'request-promise';
+import console from 'console';
 
-var fs = Promise.promisifyAll(require('fs'));
+import Promise from 'bluebird';
+let fs = Promise.promisifyAll(require('fs'));
+import request from 'request-promise';
 
 import {pathogen} from '../settings';
 
+let isFileNotFoundError = (err) => err instanceof Error && err.code === 'ENOENT';
+
+function readLocalPathogen() {
+    return fs.readFileAsync(pathogen.target, 'utf8').catch(reason => {
+        return isFileNotFoundError(reason) ? null : Promise.reject(reason);
+    });
+}
+
+function writePathogen(data) {
+    return fs.writeFileAsync(pathogen.target, data);
+}
 
 function updateVimPathogen() {
-    return request(pathogen.src)
-        .then((data) => { 
-            return fs.writeFileAsync(pathogen.target, data);
-        })
-        .then(() => console.log('Updated vim pathogen'));
+    let local_pathogen = readLocalPathogen();
+    let remote_pathogen = request(pathogen.src);
+    return Promise.all([local_pathogen, remote_pathogen])
+        .then(([local_data, data]) => {
+            if (local_data !== data) {
+                return writePathogen(data).then(() => {
+                    console.log('Updated vim pathogen.');
+                });
+            }
+            else {
+                console.log('Pathogen is up-to-date.');
+            }
+        });
 }
 
 export default function() {
