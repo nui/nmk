dd41b4821beb6bade06f5aeb7331b29054181944
Nui Narongwet <narongwet.m@gmail.com>
2016-06-25 12:01:38 +0700

    Add function to load nvm, pyenv, and rbenv

--- zsh/zshrc.src/30-autoload.zsh
+++ zsh/zshrc.src/30-autoload.zsh
@@ -1,24 +1,38 @@
-# Autoload tools
+# Detect & load version managers
 () {
     local nvm_hook_file="$HOME/.nvm/nvm.sh"
+    typeset -a managers=()
+    [[ -e $HOME/.nvm/nvm.sh ]] && {
+        managers+=(nvm)
+        function use-nvm() {
+            source $HOME/.nvm/nvm.sh
+        }
+    }
+    (( ${+commands[pyenv]} )) && {
+        managers+=(pyenv)
+        function use-pyenv() {
+            eval "$(pyenv init -)"
+            [[ ${$(pyenv commands)[(r)virtualenvwrapper]} == virtualenvwrapper ]] \
+                && pyenv virtualenvwrapper
+        }
+    }
+    (( ${+commands[rbenv]} )) && {
+        managers+=(rbenv)
+        function use-rbenv() {
+            eval "$(rbenv init -)"
+        }
+    }
     if [[ $NMK_AUTOLOAD != false ]]; then
-        # set default value if nmk_load_tools is unset
-        if ! (($+nmk_load_tools)); then
-            typeset -ga nmk_load_tools
-            [[ -e $nvm_hook_file ]] && nmk_load_tools+=(nvm)
-            hash pyenv 2> /dev/null && nmk_load_tools+=(pyenv)
-            hash rbenv 2> /dev/null && nmk_load_tools+=(rbenv)
+        # set default value if nmk_version_managers is unset
+        if ! (($+nmk_version_managers)); then
+            typeset -ga nmk_version_managers
+            nmk_version_managers=($managers)
         fi
-        for tool in $nmk_load_tools; do
-            case $tool in
-                nvm )
-                    source $nvm_hook_file ;;
-                pyenv )
-                    eval "$(pyenv init -)"
-                    [[ ${$(pyenv commands)[(r)virtualenvwrapper]} == virtualenvwrapper ]] \
-                        && pyenv virtualenvwrapper ;;
-                rbenv )
-                    eval "$(rbenv init -)" ;;
+        for manager in $nmk_version_managers; do
+            case $manager in
+                nvm ) use-nvm; unfunction use-nvm ;;
+                pyenv ) use-pyenv; unfunction use-pyenv ;;
+                rbenv ) use-rbenv; unfunction use-rbenv ;;
             esac
         done
     fi
