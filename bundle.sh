07c7cce1acb63cb634c75fd1c17c74f9d86e8301
Nui Narongwet <narongwet.m@gmail.com>
2016-10-01 21:16:49 +0700

    Refactor bundle script

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,7 @@
 set -x
 
 if ! [ -e bin/nmk -a -e bundle.sh ]; then
-    >&2 echo "This script need to run from inside NMK directory"
+    >&2 echo "This script need to run from NMK root directory"
     exit 1
 fi
 
@@ -12,8 +12,8 @@ if [ -e zsh/.zcompdump -o -e zsh/.zsh_history -o -d node_modules ]; then
     exit 1
 fi
 
-rm -f bundle.sh
-git --no-pager log -3 --oneline --color=never > LAST_COMMITS
+ORIG_PWD=$PWD
+
 date --rfc-3339=seconds > BUILD_TIME
 # add uninstall script
 cat > uninstall.sh << 'EOF'
@@ -21,22 +21,19 @@ cat > uninstall.sh << 'EOF'
 set -e
 find . -name '*.pyc' -exec rm -f {} +
 <.bundle-files xargs --null rm
-<.bundle-dirs xargs --null rmdir --ignore-fail-on-non-empty
-exec rm -f .bundle-dirs
 EOF
+rm -f bundle.sh
 
-TMPTAR=$(mktemp --suffix=.tar.gz)
-TMPDIR=$(mktemp -d)
-OUTTAR="$(dirname -- "$(dirname -- "$(readlink -f -- "$0")")")/nmk.tar.xz"
-tar -caf $TMPTAR --exclude-vcs --exclude-from=bundle.exclude .
-cd $TMPDIR && tar -xf $TMPTAR
+OUT_TAR=$ORIG_PWD/nmk.tar.xz
+TMP_DIR=$(mktemp -d)
 
+tar -c --exclude-vcs --exclude-from=bundle.exclude . | tar -x -C "$TMP_DIR"
+cd $TMP_DIR
+# create a list of files
 find . ! -type d -print0 | sort --reverse --zero-terminated > .bundle-files
-find . -mindepth 1 -type d -print0 | sort --reverse --zero-terminated > .bundle-dirs
-
-# unset write permission to get warning message on update file
+# unset write permission to get warning message on update read-only files
 find . -type f -exec chmod ugo-w {} +
+tar -caf "$OUT_TAR" --owner=0 --group=0 --mtime='' --transform 's#^\./#.nmk/#' .
 
-tar -caf "$OUTTAR" --owner=0 --group=0 --mtime='' --transform 's#^\./#.nmk/#' .
-rm -rf $TMPTAR $TMPDIR
+rm -rf $TMP_DIR
 
