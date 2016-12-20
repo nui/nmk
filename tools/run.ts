9a04efc74421abc33148fa72695aff064ae4fc52
Nui Narongwet <narongwet.m@gmail.com>
2016-12-20 21:47:49 +0700

    Compile to es6 and minor update in run.ts

--- tools/run.ts
+++ tools/run.ts
@@ -1,8 +1,9 @@
-function loadTool(name) {
+function loadTool(name: string) {
     let path = `./${name}`;
     return require(path).default;
 }
 
 if (process.mainModule.children.length === 0 && process.argv.length > 2) {
-    loadTool(process.argv[2])();
+    const args = process.argv.slice(3);
+    loadTool(process.argv[2]).apply(null, args);
 }
