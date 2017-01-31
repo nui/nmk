49ecf6a53644f8858451da98fff3842a07f73799
Nui Narongwet <narongwet.m@gmail.com>
2017-01-31 20:34:30 +0700

    Powershell lol alias support arguments

--- ms/Microsoft.PowerShell_profile.ps1
+++ ms/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 ﻿New-Alias grrr git-reset-to-remote-branch.ps1
 
-function _NMK_GIT_LOG_ONE_LINE { git log --oneline --decorate --graph --color=auto }
+function _NMK_GIT_LOG_ONE_LINE { git log --oneline --decorate --graph --color=auto $args }
 New-Alias lol _NMK_GIT_LOG_ONE_LINE
 
 function _NMK_GIT_PULL_REBASE { git pull --rebase $args }
