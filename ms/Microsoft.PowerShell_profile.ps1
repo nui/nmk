6249be4fa1481255ad551f1f687e17a2e77817e7
Nui Narongwet <narongwet.m@gmail.com>
2017-01-31 11:20:45 +0700

    Add powershell gpr alias

--- ms/Microsoft.PowerShell_profile.ps1
+++ ms/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,6 @@
 
 function _NMK_GIT_LOG_ONE_LINE { git log --oneline --decorate --graph --color=auto }
 New-Alias lol _NMK_GIT_LOG_ONE_LINE
+
+function _NMK_GIT_PULL_REBASE { git pull --rebase $args }
+New-Alias gpr _NMK_GIT_PULL_REBASE
