fbc759a0b94dbc8432c1c5f49aaa636eca1fa2c7
Nui Narongwet <narongwet.m@gmail.com>
2017-04-01 23:03:03 +0700

    Refactor nmk.py

--- bin/nmk.py
+++ bin/nmk.py
@@ -12,6 +12,7 @@ import argparse
 import six
 
 UNICODE_NAME = 'en_US.UTF-8'
+ENV = os.environ
 
 
 def build_parser():
@@ -123,18 +124,18 @@ def is_virtualenv_bin(path):
     return all((os.path.exists(p) for p in virtualenv_files))
 
 
-def setup_path(env, nmk_dir):
+def setup_path(nmk_dir):
     """
     Setup PATH environment.
       - get rid of <virtualenv>/bin
       - prepend NMK_DIR/bin and NMK_DIR/local/bin
       - remove duplicate paths
     """
-    paths = [d for d in env['PATH'].split(os.pathsep) if not is_virtualenv_bin(d)]
+    paths = [d for d in ENV['PATH'].split(os.pathsep) if not is_virtualenv_bin(d)]
     paths.insert(0, os.path.join(nmk_dir, 'local', 'bin'))
     paths.insert(0, os.path.join(nmk_dir, 'bin'))
     unique_paths = filter_duplicate_values(paths)
-    env['PATH'] = os.pathsep.join(unique_paths)
+    ENV['PATH'] = os.pathsep.join(unique_paths)
 
 
 def check_dependencies():
@@ -162,11 +163,11 @@ def is_inside_docker():
     return in_docker
 
 
-def setup_terminal(env, args, tmux_dir):
+def setup_terminal(args):
     support_256color = any((
         args.force256color,
-        env.get('TERM') in ('cygwin', 'gnome-256color', 'linux', 'putty', 'screen-256color', 'xterm-256color'),
-        env.get('COLORTERM') in ('gnome-terminal', 'rxvt-xpm', 'xfce4-terminal'),
+        ENV.get('TERM') in ('cygwin', 'gnome-256color', 'linux', 'putty', 'screen-256color', 'xterm-256color'),
+        ENV.get('COLORTERM') in ('gnome-terminal', 'rxvt-xpm', 'xfce4-terminal'),
         args.autofix and is_inside_docker(),
     ))
 
@@ -175,54 +176,52 @@ def setup_terminal(env, args, tmux_dir):
         terminal = 'screen-256color'
     else:
         terminal = 'screen'
-    env['NMK_TMUX_DEFAULT_TERMINAL'] = terminal
-    env['NMK_TMUX_256_COLOR'] = "1" if use_256color else "0"
+    ENV['NMK_TMUX_DEFAULT_TERMINAL'] = terminal
+    ENV['NMK_TMUX_256_COLOR'] = "1" if use_256color else "0"
 
 
-def setup_environment(env, args, nmk_dir):
+def setup_environment(args, nmk_dir):
     initvim = os.path.join(nmk_dir, 'vim/init.vim')
     zdotdir = os.path.join(nmk_dir, 'zsh')
 
-    env['NMK_AUTOLOAD'] = str(args.autoload).lower()
-    env['NMK_DIR'] = nmk_dir
-    env['NMK_IGNORE_LOCAL'] = str(not args.local_config).lower()
-    env['NMK_TMUX_DEFAULT_SHELL'] = whence('zsh')
-    env['NMK_TMUX_DETACH_ON_DESTROY'] = args.detach_on_destroy
-    env['NMK_TMUX_HISTORY'] = os.path.join(nmk_dir, 'tmux', '.tmux_history')
-    env['VIMINIT'] = 'source {0}'.format(initvim.replace(' ', r'\ '))
-    env['ZDOTDIR'] = zdotdir
+    ENV['NMK_AUTOLOAD'] = str(args.autoload).lower()
+    ENV['NMK_DIR'] = nmk_dir
+    ENV['NMK_IGNORE_LOCAL'] = str(not args.local_config).lower()
+    ENV['NMK_TMUX_DEFAULT_SHELL'] = whence('zsh')
+    ENV['NMK_TMUX_DETACH_ON_DESTROY'] = args.detach_on_destroy
+    ENV['NMK_TMUX_HISTORY'] = os.path.join(nmk_dir, 'tmux', '.tmux_history')
+    ENV['VIMINIT'] = 'source {0}'.format(initvim.replace(' ', r'\ '))
+    ENV['ZDOTDIR'] = zdotdir
 
-    if 'VIRTUAL_ENV' in env:
-        del env['VIRTUAL_ENV']
+    if 'VIRTUAL_ENV' in ENV:
+        del ENV['VIRTUAL_ENV']
 
-    if args.unicode or (args.autofix and 'LANG' not in env):
-        env['LANG'] = UNICODE_NAME
+    if args.unicode or (args.autofix and 'LANG' not in ENV):
+        ENV['LANG'] = UNICODE_NAME
 
     if args.force_unicode:
-        env['LC_ALL'] = UNICODE_NAME
+        ENV['LC_ALL'] = UNICODE_NAME
 
 
-def setup_prefer_editor(env):
+def setup_prefer_editor():
     prefer_editors = ('nvim', 'vim')
-    if 'EDITOR' not in env:
+    if 'EDITOR' not in ENV:
         for prog in prefer_editors:
             if whence(prog):
-                env['EDITOR'] = prog
+                ENV['EDITOR'] = prog
                 break
 
 
-def add_local_library(env, nmk_dir):
+def add_local_library(nmk_dir):
+    LD_LIBRARY_PATH = 'LD_LIBRARY_PATH'
+
     local_lib_dir = os.path.join(nmk_dir, 'local', 'lib')
     if os.path.isdir(local_lib_dir):
-        library_path = env.get('LD_LIBRARY_PATH', '')
-        if len(library_path) > 1:
-            library_paths = library_path.split(os.pathsep)
-        else:
-            library_paths = []
+        library_path = ENV.get(LD_LIBRARY_PATH)
+        library_paths = library_path.split(os.pathsep) if library_path else []
         library_paths.insert(0, local_lib_dir)
-        new_library_path = os.pathsep.join(library_paths)
-        env['LD_LIBRARY_PATH'] = new_library_path
-        logging.debug('LD_LIBRARY_PATH = {0}'.format(new_library_path))
+        ENV[LD_LIBRARY_PATH] = os.pathsep.join(library_paths)
+        logging.debug(LD_LIBRARY_PATH + ' = ' + ENV[LD_LIBRARY_PATH])
 
 
 def find_tmux_version():
@@ -251,8 +250,8 @@ def is_socket_exist(socket):
     return exists
 
 
-def exec_tmux(tmux_dir, args):
-    conf = get_tmux_conf(tmux_dir)
+def exec_tmux(args, tmux_dir):
+    conf = os.path.relpath(get_tmux_conf(tmux_dir))
 
     params = ('tmux',)
     # Use default socket unless socket name is specified.
@@ -282,13 +281,13 @@ def main():
     setup_logging(debug=args.debug)
     nmk_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     tmux_dir = os.path.join(nmk_dir, 'tmux')
-    setup_path(env=os.environ, nmk_dir=nmk_dir)
+    setup_path(nmk_dir=nmk_dir)
     check_dependencies()
-    setup_terminal(env=os.environ, args=args, tmux_dir=tmux_dir)
-    setup_environment(env=os.environ, args=args, nmk_dir=nmk_dir)
-    setup_prefer_editor(env=os.environ)
-    add_local_library(env=os.environ, nmk_dir=nmk_dir)
-    exec_tmux(tmux_dir=tmux_dir, args=args)
+    setup_terminal(args=args)
+    setup_environment(args=args, nmk_dir=nmk_dir)
+    setup_prefer_editor()
+    add_local_library(nmk_dir=nmk_dir)
+    exec_tmux(args=args, tmux_dir=tmux_dir)
 
 
 if __name__ == '__main__':
