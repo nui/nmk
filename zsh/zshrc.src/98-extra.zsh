413b91ad3162a7fd9e162a1fcf3b1874799cdeff
Nui Narongwet <narongwet.m@gmail.com>
2017-04-10 23:21:28 +0700

    Remove never used NMK_ZSHRC_EXTRA

--- zsh/zshrc.src/98-extra.zsh
+++ zsh/zshrc.src/98-extra.zsh
@@ -1,8 +1,4 @@
 if [[ $NMK_IGNORE_LOCAL != true ]]; then
     [[ -e $ZDOTDIR/zshrc.extra ]] && source $ZDOTDIR/zshrc.extra
     for file ($ZDOTDIR/zshrc.extra.d/*.zsh(N)) {source $file}
-    if [[ -e $NMK_ZSHRC_EXTRA ]]; then
-        >&2 print -- "Zsh: load extra configuration from $NMK_ZSHRC_EXTRA"
-        source $NMK_ZSHRC_EXTRA
-    fi
 fi
