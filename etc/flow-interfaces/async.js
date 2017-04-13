943731b2626395cb3622df4ae294b8a02c9b4d68
Nui Narongwet <narongwet.m@gmail.com>
2017-04-13 14:37:39 +0700

    Remove flow interfaces

--- etc/flow-interfaces/async.js
+++
@@ -1,5 +0,0 @@
-declare module async {
-    declare function map<T>(arr: Array<T>, iteratee: (item: T, iterCb: Function) => void, callback?: (err: any, result: Array<any>) => void): void;
-    declare function each<T>(arr: Array<T>, iteratee: (item: T, iterCb: Function) => void, callback?: (err: any) => void): void;
-    declare function asyncify(fn: Function): Function;
-}
