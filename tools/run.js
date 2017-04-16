9056a4502d799e1dc447ce2647ed58ec009ce2b8
Nui Narongwet <narongwet.m@gmail.com>
2017-04-16 15:20:43 +0700

    Replace Function.apply with array spread. No need node 4 support

--- tools/run.js
+++ tools/run.js
@@ -4,5 +4,5 @@ const update = require('./update');
 if (process.argv.length > 2) {
     const [tool, ...args] = process.argv.slice(2);
     let tools = {watch, update};
-    tools[tool].apply(null, args);
+    tools[tool](...args)
 }
