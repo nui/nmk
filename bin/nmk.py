48b0b2e2697acfebde942562d25d1157b9cc4b0d
Nui Narongwet <narongwet.m@gmail.com>
2017-05-28 12:15:42 +0700

    Refactor nmk.py

--- bin/nmk.py
+++ bin/nmk.py
@@ -2,6 +2,8 @@
 Run on Python 2.6.6 and later
 """
 
+from os import environ as env
+from os import path
 import logging
 import os
 import subprocess
@@ -12,7 +14,6 @@ import argparse
 import six
 
 UNICODE_NAME = 'en_US.UTF-8'
-ENV = os.environ
 
 
 def build_parser():
@@ -71,34 +72,34 @@ def build_parser():
     return parser
 
 
-def is_exec(path):
-    return os.path.isfile(path) and os.access(path, os.X_OK)
+def is_exec(p):
+    return path.isfile(p) and os.access(p, os.X_OK)
 
 
 def whence(program):
     """
     :return: path to program or None
     """
-    head, _ = os.path.split(program)
+    head, _ = path.split(program)
     # if 'program' is absolute or relative path
     if head and is_exec(program):
         return program
     # if 'program' is just a name, for example, zsh
     else:
         # return absolute path to 'program'
-        for d in os.environ["PATH"].split(os.pathsep):
-            f = os.path.join(d, program)
+        for d in env["PATH"].split(os.pathsep):
+            f = path.join(d, program)
             if is_exec(f):
                 return f
         return None
 
 
-def filter_duplicate_values(collection):
-    unique = []
-    for item in collection:
-        if item not in unique:
-            unique.append(item)
-    return unique
+def filter_duplicate_values(iterable):
+    seen = set()
+    for item in iterable:
+        if item not in seen:
+            yield item
+            seen.add(item)
 
 
 def check_output(args):
@@ -107,7 +108,6 @@ def check_output(args):
     """
     stdout = tempfile.TemporaryFile()
     subprocess.call(args, stdout=stdout)
-    stdout.flush()
     stdout.seek(0)
     return stdout.read()
 
@@ -123,9 +123,9 @@ def python_info():
     logging.debug('python ' + sys.version)
 
 
-def is_virtualenv_bin(path):
-    virtualenv_files = (os.path.join(path, name) for name in ('activate', 'python'))
-    return all((os.path.exists(p) for p in virtualenv_files))
+def is_virtualenv_bin(p):
+    virtualenv_files = (path.join(p, name) for name in ('activate', 'python'))
+    return all((path.exists(p) for p in virtualenv_files))
 
 
 def setup_path(nmk_dir):
@@ -135,14 +135,15 @@ def setup_path(nmk_dir):
       - prepend NMK_DIR/bin and NMK_DIR/local/bin
       - remove duplicate paths
     """
-    paths = [d for d in ENV['PATH'].split(os.pathsep) if not is_virtualenv_bin(d)]
-    paths.insert(0, os.path.join(nmk_dir, 'local', 'bin'))
-    paths.insert(0, os.path.join(nmk_dir, 'bin'))
-    unique_paths = filter_duplicate_values(paths)
-    ENV['PATH'] = os.pathsep.join(unique_paths)
-    logging.debug('PATH:' + ENV['PATH'])
+    paths = [
+        path.join(nmk_dir, 'bin'),
+        path.join(nmk_dir, 'local', 'bin')
+    ] + [d for d in env['PATH'].split(os.pathsep) if not is_virtualenv_bin(d)]
+    unique_paths = list(filter_duplicate_values(paths))
     for p in unique_paths:
         logging.debug('PATH:' + p)
+    env['PATH'] = os.pathsep.join(unique_paths)
+    logging.debug('PATH:' + env['PATH'])
 
 
 def check_dependencies():
@@ -161,7 +162,7 @@ def parse_cgroup(cgroup_file):
 
 def is_inside_docker():
     cgroup_file = '/proc/1/cgroup'
-    if os.path.exists(cgroup_file):
+    if path.exists(cgroup_file):
         control_groups = parse_cgroup(cgroup_file)
         in_docker = all((g != '/' for g in control_groups))
     else:
@@ -173,8 +174,8 @@ def is_inside_docker():
 def setup_terminal(args):
     support_256color = any((
         args.force256color,
-        ENV.get('TERM') in ('cygwin', 'gnome-256color', 'putty', 'screen-256color', 'xterm-256color'),
-        ENV.get('COLORTERM') in ('gnome-terminal', 'rxvt-xpm', 'xfce4-terminal'),
+        env.get('TERM') in ('cygwin', 'gnome-256color', 'putty', 'screen-256color', 'xterm-256color'),
+        env.get('COLORTERM') in ('gnome-terminal', 'rxvt-xpm', 'xfce4-terminal'),
         args.autofix and is_inside_docker(),
     ))
 
@@ -183,41 +184,41 @@ def setup_terminal(args):
         terminal = 'screen-256color'
     else:
         terminal = 'screen'
-    ENV['NMK_TMUX_DEFAULT_TERMINAL'] = terminal
-    ENV['NMK_TMUX_256_COLOR'] = "1" if use_256color else "0"
+    env['NMK_TMUX_DEFAULT_TERMINAL'] = terminal
+    env['NMK_TMUX_256_COLOR'] = "1" if use_256color else "0"
 
 
 def setup_environment(args, nmk_dir):
-    initvim = os.path.join(nmk_dir, 'vim/init.vim')
-    zdotdir = os.path.join(nmk_dir, 'zsh')
+    initvim = path.join(nmk_dir, 'vim/init.vim')
+    zdotdir = path.join(nmk_dir, 'zsh')
 
-    ENV['NMK_AUTOLOAD'] = str(args.autoload).lower()
-    ENV['NMK_DIR'] = nmk_dir
-    ENV['NMK_TMUX_DEFAULT_SHELL'] = whence('zsh')
-    ENV['NMK_TMUX_DETACH_ON_DESTROY'] = args.detach_on_destroy
-    ENV['NMK_TMUX_HISTORY'] = os.path.join(nmk_dir, 'tmux', '.tmux_history')
-    ENV['VIMINIT'] = 'source {0}'.format(initvim.replace(' ', r'\ '))
-    ENV['ZDOTDIR'] = zdotdir
+    env['NMK_AUTOLOAD'] = str(args.autoload).lower()
+    env['NMK_DIR'] = nmk_dir
+    env['NMK_TMUX_DEFAULT_SHELL'] = whence('zsh')
+    env['NMK_TMUX_DETACH_ON_DESTROY'] = args.detach_on_destroy
+    env['NMK_TMUX_HISTORY'] = path.join(nmk_dir, 'tmux', '.tmux_history')
+    env['VIMINIT'] = 'source {0}'.format(initvim.replace(' ', r'\ '))
+    env['ZDOTDIR'] = zdotdir
 
-    if 'VIRTUAL_ENV' in ENV:
-        del ENV['VIRTUAL_ENV']
+    if 'VIRTUAL_ENV' in env:
+        del env['VIRTUAL_ENV']
         logging.debug('unset VIRTUAL_ENV')
 
-    if args.unicode or (args.autofix and 'LANG' not in ENV):
-        ENV['LANG'] = UNICODE_NAME
+    if args.unicode or (args.autofix and 'LANG' not in env):
+        env['LANG'] = UNICODE_NAME
         logging.debug('set LANG = ' + UNICODE_NAME)
 
     if args.force_unicode:
-        ENV['LC_ALL'] = UNICODE_NAME
+        env['LC_ALL'] = UNICODE_NAME
         logging.debug('set LC_ALL = ' + UNICODE_NAME)
 
 
 def setup_prefer_editor():
     prefer_editors = ('nvim', 'vim')
-    if 'EDITOR' not in ENV:
+    if 'EDITOR' not in env:
         for prog in prefer_editors:
             if whence(prog):
-                ENV['EDITOR'] = prog
+                env['EDITOR'] = prog
                 logging.debug('set EDITOR = ' + prog)
                 break
 
@@ -225,14 +226,14 @@ def setup_prefer_editor():
 def add_local_library(nmk_dir):
     LD_LIBRARY_PATH = 'LD_LIBRARY_PATH'
 
-    local_lib_dir = os.path.join(nmk_dir, 'local', 'lib')
-    if os.path.isdir(local_lib_dir):
-        library_path = ENV.get(LD_LIBRARY_PATH)
+    local_lib_dir = path.join(nmk_dir, 'local', 'lib')
+    if path.isdir(local_lib_dir):
+        library_path = env.get(LD_LIBRARY_PATH)
         library_paths = library_path.split(os.pathsep) if library_path else []
         library_paths.insert(0, local_lib_dir)
-        ENV[LD_LIBRARY_PATH] = os.pathsep.join(library_paths)
+        env[LD_LIBRARY_PATH] = os.pathsep.join(library_paths)
         logging.debug('prepend ' + local_lib_dir + ' to ' + LD_LIBRARY_PATH)
-        logging.debug(LD_LIBRARY_PATH + ' = ' + ENV[LD_LIBRARY_PATH])
+        logging.debug(LD_LIBRARY_PATH + ' = ' + env[LD_LIBRARY_PATH])
 
 
 def find_tmux_version():
@@ -244,8 +245,9 @@ def find_tmux_version():
 
 def get_tmux_conf(tmux_dir):
     version = find_tmux_version()
-    conf = os.path.join(tmux_dir, '{0}.conf'.format(version))
-    if not os.path.exists(conf):
+    logging.debug('using tmux {0}'.format(version))
+    conf = path.join(tmux_dir, '{0}.conf'.format(version))
+    if not path.exists(conf):
         logging.error('tmux {0} is unsupported'.format(version))
         sys.exit(1)
     return conf
@@ -262,7 +264,7 @@ def is_socket_exist(socket):
 
 
 def exec_tmux(args, tmux_dir):
-    conf = os.path.relpath(get_tmux_conf(tmux_dir))
+    conf = path.relpath(get_tmux_conf(tmux_dir))
 
     params = ('tmux',)
     # Use default socket unless socket name is specified.
@@ -278,8 +280,8 @@ def exec_tmux(args, tmux_dir):
         if tmux_args:
             params += tuple(tmux_args)
         else:
-            if 'TMUX' in ENV and not args.inception:
-                logging.error('add --inception to allow nested sessions')
+            if 'TMUX' in env and not args.inception:
+                logging.error('add --inception to allow nested tmux sessions')
                 sys.exit(1)
             params += ('attach',)
     else:
@@ -294,8 +296,8 @@ def main():
     args = build_parser().parse_args()
     setup_logging(debug=args.debug)
     python_info()
-    nmk_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    tmux_dir = os.path.join(nmk_dir, 'tmux')
+    nmk_dir = path.dirname(path.dirname(path.abspath(__file__)))
+    tmux_dir = path.join(nmk_dir, 'tmux')
     setup_path(nmk_dir=nmk_dir)
     check_dependencies()
     setup_terminal(args=args)
