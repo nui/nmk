71a6cdf2e351a69e259fe70f25b2311e4aec3ec0
Nui Narongwet <narongwet.m@gmail.com>
2018-05-05 13:22:07 +0700

    Renaming

--- tmux/bindkey.js
+++ tmux/bindkey.js
@@ -1,9 +1,8 @@
 const V26 = 2.6;
 const V27 = 2.7;
 
-let f4 = (v) => v >= V27 ? "-sZ" : "-s";
+let chooseTree = (v) => v >= V27 ? "-sZ" : "-s";
 
 exports.bindkey = (version) => ({
-    f4: f4(version)
+    chooseTree: chooseTree(version)
 });
-
