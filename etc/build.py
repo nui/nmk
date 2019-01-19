9e5453e89d411e121d7a9fd60ac78d73d08cd0e3
Nui Narongwet <narongwet.m@gmail.com>
2019-01-19 16:48:17 +0700

    refactor build.py

--- etc/build.py
+++ etc/build.py
@@ -11,7 +11,6 @@ import tempfile
 from datetime import datetime
 from pathlib import Path
 
-
 TMPDIR_SUFFIX = '.nmk-build'
 
 
@@ -35,58 +34,54 @@ def build_parser():
 
 
 def clone_repo(branch):
-    origin = 'https://github.com/nuimk/nmk.git'
-    repo_dir = Path(tempfile.mkdtemp(suffix=TMPDIR_SUFFIX))
-    common_kwargs = {
-        'stdout': sys.stdout,
-        'stderr': sys.stderr,
-        'cwd': repo_dir,
-    }
-    subprocess.run(['git', 'clone', '--quiet', '-b', branch, origin, repo_dir], stdout=sys.stdout, stderr=sys.stderr)
-    subprocess.run(['git', 'submodule', 'init'], **common_kwargs)
-    subprocess.run(['git', 'submodule', '--quiet', 'update', '--recursive'], **common_kwargs)
-    subprocess.run(['git', 'remote', 'set-url', 'origin', origin], **common_kwargs)
-    return repo_dir
+    repo = Path(tempfile.mkdtemp(suffix=TMPDIR_SUFFIX))
+    subprocess.run(['git', 'clone', '--recursive', '-b', branch,
+                    'https://github.com/nuimk/nmk.git', repo])
+    return repo
 
 
-def clone_vim_plugins(repo):
+def read_plugin_tuple(repo):
     pattern = re.compile(r'^(\w\S+)\s+(\S+)')
     plugin_file = open(repo.joinpath('vim', 'plugins'), 'rt')
-    plugin_dir = repo.joinpath('vim', 'bundle')
     for line in plugin_file.readlines():
         m = pattern.match(line)
         if m:
-            (name, url) = m.groups()
-            print(f'Cloning {name}')
-            subprocess.run(['git', 'clone', '--quiet', url, name], cwd=plugin_dir)
+            yield m.groups()
+
+
+def clone_vim_plugins(repo):
+    bundle_dir = repo.joinpath('vim', 'bundle')
+    for (name, url) in read_plugin_tuple(repo):
+        print(f'Cloning {name}')
+        subprocess.run(['git', 'clone', '--quiet', url, name], cwd=bundle_dir)
 
 
 # Generate files that need information from git
 def generate_buildinfo(repo):
-    now = datetime.now()
-    out = open(repo.joinpath('.buildinfo'), 'wt')
-
     kwargs = {
         'stdout': subprocess.PIPE,
-        'stderr': sys.stderr,
         'cwd': repo,
     }
-    lines = [f"Build on {now}", "Last 10 commits"]
-    _ = lambda proc: lines.append(proc.stdout.decode())
+
+    lines = [f"Build on {datetime.now()}", "Last 10 commits"]
+    _ = lambda p: lines.append(p.stdout.decode())
+
     cmd = ["git", "--no-pager", "log", "-n", "10", "--no-color", "--oneline", "--decorate", "--graph"]
     _(subprocess.run(cmd, **kwargs))
-    lines.append("Submodules:")
+
+    lines.append("Git submodules:")
     _(subprocess.run(["git", "submodule", 'status'], **kwargs))
+
     lines.append("Vim plugins:")
-    vim_plugin_dir = repo.joinpath('vim', 'bundle')
-    for plugin_name in os.listdir(vim_plugin_dir):
-        plugin_repo = vim_plugin_dir.joinpath(plugin_name)
-        if plugin_repo.is_dir():
-            proc = subprocess.run(['git', 'rev-parse', '--verify', 'HEAD'],
-                                  stdout=subprocess.PIPE, stderr=sys.stderr, cwd=plugin_repo)
-            lines.append(f'{proc.stdout.decode().strip()}  {plugin_name}')
-    out.write('\n'.join(lines) + '\n')
-    out.close()
+    vim_bundle_dir = repo.joinpath('vim', 'bundle')
+    for (plugin_name, _) in read_plugin_tuple(repo):
+        plugin_repo = vim_bundle_dir.joinpath(plugin_name)
+        proc = subprocess.run(['git', 'rev-parse', '--verify', 'HEAD'],
+                              stdout=subprocess.PIPE, cwd=plugin_repo)
+        head = proc.stdout.decode().strip()
+        lines.append(f'{head}  {plugin_name}')
+    with open(repo.joinpath('.buildinfo'), 'wt') as out:
+        out.write('\n'.join(lines) + '\n')
 
 
 delete_cache_files = r"""#!/bin/sh
@@ -102,49 +97,44 @@ def generate_more_files(workdir):
     # add uninstaller script
     with open(workdir.joinpath('uninstall.sh'), 'wt') as f:
         f.write(delete_cache_files)
-    create_list_files = 'find . ! -type d -print0 | sort --reverse --zero-terminated > .installed-files'
     # create a list of bundled files
+    create_list_files = 'find . ! -type d -print0 | sort --reverse --zero-terminated > .installed-files'
     subprocess.run(create_list_files, shell=True, cwd=workdir)
     # unset write permission to get warning message on update read-only files
     subprocess.run('find . -type f -exec chmod ugo-w {} +', shell=True, cwd=workdir)
 
 
-def delete_unwanted_files(repo, ignore_files):
-    ignore_files = list_ignored_files(repo)
-    (_, tmp_tar_path) = tempfile.mkstemp('.tar')
-    os.remove(tmp_tar_path)
-    tmp_tar_file = tarfile.open(tmp_tar_path, mode='x')
+def delete_unwanted_files(repo):
+    (_, archive_path) = tempfile.mkstemp('.tar')
+    os.remove(archive_path)
 
-    def tar_filter(tarinfo):
-        ignored = any((
-            tarinfo.name.endswith('.git') and tarinfo.isdir(),
-            tarinfo.name in ignore_files
-        ))
-        return tarinfo if not ignored else None
-
-    add_all_to_tar(src=repo, filter=tar_filter, archive=tmp_tar_file)
-
-    tmp_tar_file.close()
+    with tarfile.open(archive_path, mode='x') as archive:
+        def tar_filter(tarinfo):
+            ignored = any((
+                tarinfo.name.endswith('.git') and tarinfo.isdir(),
+                tarinfo.name in list_ignored_files(repo)
+            ))
+            return tarinfo if not ignored else None
+        add_all_to_tar(src=repo, filter=tar_filter, archive=archive)
     tmp_dir = Path(tempfile.mkdtemp(TMPDIR_SUFFIX))
-    subprocess.run(['tar', '-x', '-f', tmp_tar_path], cwd=tmp_dir)
-    os.remove(tmp_tar_path)
+    subprocess.run(['tar', '-x', '-f', archive_path], cwd=tmp_dir)
+    os.remove(archive_path)
     return tmp_dir
 
 
 def add_all_to_tar(archive, src, filter):
-    current_directory = Path(os.curdir).absolute()
+    cwd = Path(os.curdir).absolute()
     os.chdir(src)
     for file in os.listdir(src):
         archive.add(file, filter=filter)
-    os.chdir(current_directory)
+    os.chdir(cwd)
 
 
 def list_ignored_files(repo):
     ignore_file = repo.joinpath('.archiveignore')
-    lines = open(ignore_file, 'rt').readlines()
+    lines = [line.strip() for line in open(ignore_file, 'rt').readlines()]
     files = []
     for line in lines:
-        line = line.strip('\n')
         if line.startswith('#') or line == '':
             continue
         files.append(line)
@@ -161,29 +151,26 @@ def create_final_archive(workdir):
         tarinfo.name = str(Path('.nmk').joinpath(tarinfo.name))
         return tarinfo
 
-    (_, tar_path) = tempfile.mkstemp('.nmk.tar.gz')
-    os.remove(tar_path)
-    archive = tarfile.open(tar_path, 'x:gz')
-    add_all_to_tar(archive=archive, src=workdir, filter=tar_filter)
-    archive.close()
-    return tar_path
+    (_, archive_path) = tempfile.mkstemp('.nmk.tar.gz')
+    os.remove(archive_path)
+    with tarfile.open(archive_path, 'x:gz') as archive:
+        add_all_to_tar(archive=archive, src=workdir, filter=tar_filter)
+    return archive_path
 
 
-def generate_hash(archive):
-    out = open(archive.parent.joinpath(archive.name + '.sha256'), 'wt')
+def generate_hash(file):
     h = hashlib.sha256()
-    h.update(open(archive, 'rb').read())
-    out.write(f'{h.hexdigest()} *{archive.name}\n')
-    out.close()
+    h.update(open(file, 'rb').read())
+    with open(file.parent.joinpath(file.name + f'.{h.name}'), 'wt') as out:
+        out.write(f'{h.hexdigest()} *{file.name}\n')
 
 
 def upload(workdir):
-    prefix = ['gsutil', '-h', "Cache-Control:private, max-age=0, no-transform",
-              'cp', '-a', 'public-read']
-    cloud_path = 'gs://nmk.nuimk.com/'
+    prefix = ('gsutil',
+              '-h', 'Cache-Control:private, max-age=0, no-transform',
+              'cp', '-a', 'public-read')
     for file in ('nmk.tar.gz', 'nmk.tar.gz.sha256'):
-        subprocess.run(prefix + [workdir.joinpath(file), cloud_path + file],
-                       stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr).check_returncode()
+        subprocess.run(prefix + (workdir.joinpath(file), f'gs://nmk.nuimk.com/{file}')).check_returncode()
 
 
 def sign_archive_and_open_explorer(workdir):
@@ -205,7 +192,7 @@ def main():
     clone_vim_plugins(repo)
     generate_buildinfo(repo)
 
-    tmp_dir = delete_unwanted_files(repo, ignore_files=list_ignored_files(repo))
+    tmp_dir = delete_unwanted_files(repo)
     shutil.rmtree(repo)
     generate_more_files(tmp_dir)
     tmp_archive = create_final_archive(tmp_dir)
