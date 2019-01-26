05693ce2fed0f8878c0e1bdc01fa8f1f77c36c35
Nui Narongwet <narongwet.m@gmail.com>
2019-01-27 03:08:15 +0700

    cat /etc/motd if exist

--- etc/ssh-entrypoint.sh
+++ etc/ssh-entrypoint.sh
@@ -13,6 +13,10 @@ if [ -e /var/run/motd.dynamic ]; then
     cat /var/run/motd.dynamic
 fi
 
+if [ -e /etc/motd ]; then
+    cat /etc/motd
+fi
+
 # Make sure that byobu doesn't take over our login shell
 exec env BYOBU_DISABLE=1 $_LOGIN_SHELL -l -c 'exec ${NMK_DIR:-~/.nmk}/bin/nmk -l'
 # vi: ft=sh
