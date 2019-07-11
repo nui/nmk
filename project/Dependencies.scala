388956ab3545c3347f4741cb49588b3a3bf375a4
Nui Narongwet <narongwet.m@gmail.com>
2019-07-12 03:49:38 +0700

    upgrade scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val guice = "com.google.inject" % "guice" % "4.2.2"
-  lazy val scalaGuice = "net.codingwell" %% "scala-guice" % "4.2.2"
-  lazy val jackson = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.6"
+  lazy val scalaGuice = "net.codingwell" %% "scala-guice" % "4.2.5"
+  lazy val jackson = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.9"
 }
