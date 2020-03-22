4089b9f1de44e3f4dfa5cf45af3f722c34a726c0
Nui Narongwet <narongwet.m@gmail.com>
2020-03-22 14:33:44 +0700

    scala: migrate deprecate method

--- src/main/scala/nmk/Zsh.scala
+++ src/main/scala/nmk/Zsh.scala
@@ -4,12 +4,12 @@ import java.nio.file._
 
 import javax.inject.Singleton
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.collection.mutable.ListBuffer
 
 @Singleton
 class Zsh {
-  def render(nmkDir: String) = {
+  def render(nmkDir: String): String = {
     val zshSrcDir = Paths.get(nmkDir, "zsh", "src", "zshrc")
     val paths = ListBuffer.empty[Path]
     Files.list(zshSrcDir).forEach(paths.append(_))
