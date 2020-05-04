42ddbfe7e6ddf517783113c8905648374f71bdd1
Nui Narongwet <narongwet.m@gmail.com>
2020-05-05 01:30:17 +0700

    Add tmux 3.1b

--- src/main/scala/nmk/tmux/Version.scala
+++ src/main/scala/nmk/tmux/Version.scala
@@ -36,7 +36,9 @@ object Version {
 
   case object V31a extends Version("3.1a")
 
-  private val allVersions = List(V21, V22, V23, V24, V25, V26, V27, V28, V29, V29a, V30, V30a, V31, V31a)
+  case object V31b extends Version("3.1b")
+
+  private val allVersions = List(V21, V22, V23, V24, V25, V26, V27, V28, V29, V29a, V30, V30a, V31, V31a, V31b)
 
   private val unsupportedVersions = List(V24)
 
