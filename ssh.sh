af68dc24b8632470f2ec325299f18f95d4a40b47
Nui Narongwet <narongwet.m@gmail.com>
2020-08-01 14:51:07 +0700

    More strict ssh login

--- ssh.sh
+++ ssh.sh
@@ -1,16 +1,16 @@
 # POSIX compatible script
 
-try_exec_login_shell() {
-    if [ -e "$1" ]; then
+try_zsh_login() {
+    if [ -x "$1" -a -f "$1" ]; then
         exec "$1" --motd --login
     fi
 }
 
-if [ -e "$HOME" ]; then
-    try_exec_login_shell "$HOME/.nmk/bin/nmk"
-    try_exec_login_shell "$HOME/bin/nmk"
+if [ -d "$HOME" ]; then
+    try_zsh_login "$HOME/.nmk/bin/nmk"
+    try_zsh_login "$HOME/bin/nmk"
 fi
-try_exec_login_shell "/usr/local/bin/nmk"
+try_zsh_login "/usr/local/bin/nmk"
 
 # This may not work on dash, but who use dash as a login shell?
 exec -l $SHELL
