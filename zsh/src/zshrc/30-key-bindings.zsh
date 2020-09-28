110f4f0bb715d43a605ecc2989a66180c65e3977
Nui Narongwet <narongwet.m@gmail.com>
2020-09-28 15:17:58 +0700

    Don't enter copy mode outside tmux

--- zsh/src/zshrc/30-key-bindings.zsh
+++ zsh/src/zshrc/30-key-bindings.zsh
@@ -42,7 +42,7 @@
     # use emacs keybindings
     bindkey -e
 
-    if [[ -n $NMK_TMUX_VERSION ]]; then
+    if [[ -n $TMUX && -n $NMK_TMUX_VERSION ]]; then
         # PageUp to enter copy mode
         _nmk-tmux-copy-mode() tmux copy-mode -eu
         zle -N _nmk-tmux-copy-mode
