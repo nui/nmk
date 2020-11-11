b3dd91215ccb59c5317970c9d86f98151f7eca8c
Nui Narongwet <narongwet.m@gmail.com>
2020-11-12 04:49:31 +0700

    Keep it simple

--- nmk/src/bin/nmk/terminal.rs
+++ nmk/src/bin/nmk/terminal.rs
@@ -1,16 +1,9 @@
 use std::env;
-use std::ffi::{OsStr, OsString};
-use std::ops::Deref;
 
 use nmk::container;
 
-fn slice_contains_term<T: Deref<Target = OsStr>>(slice: &[&str], term: Option<T>) -> bool {
-    term.as_deref()
-        .and_then(OsStr::to_str)
-        .map_or(false, |s| slice.contains(&s))
-}
-
-fn is_256_color_term(term: Option<OsString>) -> bool {
+fn is_256_color_term<T: AsRef<str>>(term: T) -> bool {
+    let term = term.as_ref();
     const TERM_LIST: &[&str] = &[
         "cygwin",
         "gnome-256color",
@@ -18,17 +11,18 @@ fn is_256_color_term(term: Option<OsString>) -> bool {
         "screen-256color",
         "xterm-256color",
     ];
-    slice_contains_term(TERM_LIST, term)
+    TERM_LIST.contains(&term)
 }
 
-fn is_256_color_colorterm(term: Option<OsString>) -> bool {
+fn is_256_color_colorterm<T: AsRef<str>>(term: T) -> bool {
+    let term = term.as_ref();
     const COLORTERM_LIST: &[&str] = &["gnome-terminal", "rxvt-xpm", "xfce4-terminal"];
-    slice_contains_term(COLORTERM_LIST, term)
+    COLORTERM_LIST.contains(&term)
 }
 
 pub fn support_256_color() -> bool {
-    is_256_color_term(env::var_os("TERM"))
-        || is_256_color_colorterm(env::var_os("COLORTERM"))
+    env::var("TERM").iter().any(is_256_color_term)
+        || env::var("COLORTERM").iter().any(is_256_color_colorterm)
         || container::is_containerized()
 }
 
@@ -36,29 +30,23 @@ pub fn support_256_color() -> bool {
 mod tests {
     use super::*;
 
-    fn input(i: &str) -> Option<OsString> {
-        Some(OsString::from(i))
-    }
-
     #[test]
     fn test_is_256_term() {
-        assert!(is_256_color_term(input("cygwin")));
-        assert!(is_256_color_term(input("gnome-256color")));
-        assert!(is_256_color_term(input("putty")));
-        assert!(is_256_color_term(input("screen-256color")));
-        assert!(is_256_color_term(input("xterm-256color")));
-        assert!(!is_256_color_term(input("linux")));
-        assert!(!is_256_color_term(input("")));
-        assert!(!is_256_color_term(None));
+        assert!(is_256_color_term("cygwin"));
+        assert!(is_256_color_term("gnome-256color"));
+        assert!(is_256_color_term("putty"));
+        assert!(is_256_color_term("screen-256color"));
+        assert!(is_256_color_term("xterm-256color"));
+        assert!(!is_256_color_term("linux"));
+        assert!(!is_256_color_term(""));
     }
 
     #[test]
     fn test_is_256_colorterm() {
-        assert!(is_256_color_colorterm(input("gnome-terminal")));
-        assert!(is_256_color_colorterm(input("rxvt-xpm")));
-        assert!(is_256_color_colorterm(input("xfce4-terminal")));
-        assert!(!is_256_color_colorterm(input("unknown")));
-        assert!(!is_256_color_colorterm(input("")));
-        assert!(!is_256_color_colorterm(None));
+        assert!(is_256_color_colorterm("gnome-terminal"));
+        assert!(is_256_color_colorterm("rxvt-xpm"));
+        assert!(is_256_color_colorterm("xfce4-terminal"));
+        assert!(!is_256_color_colorterm("unknown"));
+        assert!(!is_256_color_colorterm(""));
     }
 }
