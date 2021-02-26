98080642172493b379c97dd62f73bba362987097
Nui Narongwet <narongwet.m@gmail.com>
2021-02-26 17:38:19 +0700

    Add comment

--- etc/macos/nmk.jetbrains.toolbox.launcher.sh
+++ etc/macos/nmk.jetbrains.toolbox.launcher.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh 
+# Change stack size (should only apply to macos?)
+# ulimit -a
+# ulimit -s 32768
 exec "/Applications/JetBrains Toolbox.app/Contents/MacOS/jetbrains-toolbox"
