20c1b6228ae082c4e9df2c4ee91a3c375d78758e
Nui Narongwet <narongwet.m@gmail.com>
2021-03-07 22:29:57 +0700

    Remove trailing whitespace

--- etc/macos/nmk.jetbrains.toolbox.launcher.sh
+++ etc/macos/nmk.jetbrains.toolbox.launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh 
+export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
 # Change stack size (should only apply to macos?)
 # ulimit -a
 # ulimit -s 32768
