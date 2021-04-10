4d4aae16dfd776b6b578c482b080bc53667a0c3d
Nui Narongwet <narongwet.m@gmail.com>
2021-04-10 21:33:50 +0700

    Avoid String allocation and rename parts -> components

--- nmk/src/nmk/human_time.rs
+++ nmk/src/nmk/human_time.rs
@@ -1,3 +1,4 @@
+use std::fmt::Write;
 use std::fmt::{self, Display};
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -5,7 +6,7 @@ const MINUTE_SECONDS: u64 = 60;
 const HOUR_SECONDS: u64 = 60 * MINUTE_SECONDS;
 const DAY_SECONDS: u64 = 24 * HOUR_SECONDS;
 
-const ALL_PARTS: u8 = u8::MAX;
+const ALL_COMPONENTS: u8 = u8::MAX;
 
 #[derive(Copy, Clone)]
 pub struct HumanTime(u64);
@@ -43,15 +44,18 @@ impl HumanTime {
         self.0 % MINUTE_SECONDS
     }
 
-    pub fn to_human(self, max_num_parts: u8) -> String {
-        self.parts()
-            .take(max_num_parts.into())
-            .map(|v| v.to_string())
-            .collect::<Vec<_>>()
-            .join(" ")
+    pub fn to_human(self, num_components: u8) -> String {
+        let mut components = self.components().take(num_components.into());
+        // We could also collect to Vec<String> then call .join(" ")
+        // We code this way to show an alternative style which avoid String allocation.
+        let mut out = components.next().map(|c| c.to_string()).unwrap_or_default();
+        for c in components {
+            write!(&mut out, " {}", c).expect("Infallible string write");
+        }
+        out
     }
 
-    pub fn parts(self) -> Components {
+    pub fn components(self) -> Components {
         Components::new(self)
     }
 }
@@ -161,7 +165,7 @@ impl Iterator for Components {
 
 impl Display for HumanTime {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_human(ALL_PARTS))
+        f.write_str(&self.to_human(ALL_COMPONENTS))
     }
 }
 
@@ -183,7 +187,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_to_human() {
         assert_eq!(HumanTime(1).to_human(2), "1s");
         assert_eq!(HumanTime(10).to_human(2), "10s");
         assert_eq!(HumanTime(59).to_human(2), "59s");
