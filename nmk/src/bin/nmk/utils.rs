67f0c1b5a7b454bf78aae40ea04926829d8f042f
Nui Narongwet <narongwet.m@gmail.com>
2021-04-15 16:05:58 +0700

    Show unit in --usage

--- nmk/src/bin/nmk/utils.rs
+++ nmk/src/bin/nmk/utils.rs
@@ -3,8 +3,8 @@ use crate::cmdline::CmdOpt;
 pub fn print_usage_time(cmd_opt: &CmdOpt) {
     let elapsed = cmd_opt.start_time.elapsed().as_millis();
     if cmd_opt.usage {
-        println!("{}", elapsed);
+        println!("{} ms.", elapsed);
     } else {
-        log::debug!("Usage time: {} ms", elapsed);
+        log::debug!("Usage time: {} ms.", elapsed);
     }
 }
