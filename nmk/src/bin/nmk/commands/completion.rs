d29e78143f8eb112ef9959d1ae16f59ce001a965
Nui Narongwet <narongwet.m@gmail.com>
2021-04-25 19:13:00 +0700

    Rename variable

--- nmk/src/bin/nmk/commands/completion.rs
+++ nmk/src/bin/nmk/commands/completion.rs
@@ -8,9 +8,9 @@ use nmk::bin_name::NMK;
 use crate::cmdline::{CmdOpt, Completion};
 
 pub fn gen_completion(completion: &Completion) {
-    let mut write: Box<dyn Write> = match completion.output {
-        Some(ref p) => Box::new(File::create(p).expect("cannot create completion output file")),
+    let mut output: Box<dyn Write> = match completion.output {
+        Some(ref p) => Box::new(File::create(p).expect("cannot create completion file")),
         None => Box::new(std::io::stdout()),
     };
-    CmdOpt::clap().gen_completions_to(NMK, completion.shell, &mut write);
+    CmdOpt::clap().gen_completions_to(NMK, completion.shell, &mut output);
 }
