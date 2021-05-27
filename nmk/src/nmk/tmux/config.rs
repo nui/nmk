e14478551bfc7317e22140454e383b57f38e7028
narongwet.m@gmail.com <narongwet.m@gmail.com>
2021-05-27 13:55:49 +0700

    use .write_all when write literal string

--- nmk/src/nmk/tmux/config.rs
+++ nmk/src/nmk/tmux/config.rs
@@ -29,8 +29,7 @@ pub fn render(w: &mut dyn Write, c: &Context, v: Version) -> io::Result<()> {
     writeln!(w, "bind-key -r C-o rotate-window")?;
     writeln!(w, "bind-key C-c command-prompt")?;
     writeln!(w, "bind-key C-l {}", LAST_SESSION)?;
-    #[allow(clippy::write_literal)]
-    writeln!(w, "{}", "bind-key C-t display-message '#{pane_tty}'")?;
+    w.write_all("bind-key C-t display-message '#{pane_tty}'\n".as_bytes())?;
     section(w, c, "Function Key Binding", |w, _| {
         for n in 1..=12 {
             writeln!(w, "bind-key -n S-F{n} send-keys F{n}", n = n)?;
