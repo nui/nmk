b843ea6a9eb4189401d12efd002f37193d71ff09
Nui Narongwet <narongwet.m@gmail.com>
2021-07-01 05:28:39 +0700

    Split function to easily test join output

--- nmk/src/nmk/arch.rs
+++ nmk/src/nmk/arch.rs
@@ -1,26 +1,13 @@
 use std::io::Write;
-use std::iter;
 use std::process::{Command, Stdio};
-
-use indoc::indoc;
 
 /// A modified version of https://sh.rustup.rs
 ///
 /// N.B. we could compress this but include-flate (0.1.3) doesn't support stable yet.
 const NMKUP_INIT_SCRIPT: &str = include_str!("../../nmkup.nuimk.com/nmkup-init.sh");
 
-const GET_ARCHITECTURE: &str = indoc! {r##"
-    get_architecture || return 1
-    echo $RETVAL
-"##};
-
 pub fn detect_current_architecture() -> crate::Result<String> {
-    let get_architecture_script = NMKUP_INIT_SCRIPT
-        .lines()
-        .take_while(|line| !line.starts_with(r##"main "$@""##))
-        .chain(iter::once(GET_ARCHITECTURE))
-        .collect::<Vec<_>>()
-        .join("\n");
+    let get_architecture_script = generate_get_architecture_script();
     let mut shell = Command::new("sh")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -31,4 +18,18 @@ pub fn detect_current_architecture() -> crate::Result<String> {
     let output = shell.wait_with_output()?;
     let arch = std::str::from_utf8(&output.stdout)?.trim().to_string();
     Ok(arch)
+}
+
+fn generate_get_architecture_script() -> String {
+    #[rustfmt::skip]
+    let get_architecture_lines = [
+        "get_architecture || return 1",
+        "echo $RETVAL",
+    ];
+    NMKUP_INIT_SCRIPT
+        .lines()
+        .take_while(|line| !line.starts_with(r##"main "$@""##))
+        .chain(get_architecture_lines)
+        .collect::<Vec<_>>()
+        .join("\n")
 }
